d0440bd6cd4e424b29957ea6d5db9c1bbe7780e5
Ghost <2528639663@qq.com>
2026-07-21 15:36:25 +0800

    gallery: edit album "测试相簿"

--- src/config/galleryConfig.ts
+++ src/config/galleryConfig.ts
@@ -15,15 +15,13 @@ export const galleryConfig: GalleryConfig = {
 		// password: 访问密码，设置后需要输入密码才能查看相册内容（可选）
 		// passwordHint: 密码提示，设置后在输入密码错误时显示（可选，需配合password使用）
 		// 每添加一个数组项就相当于添加了一个相册，记得在 public/gallery/ 目录下创建对应的子目录并放入图片
-			{
+		{
 			id: "测试相簿",
 			name: "测试相簿",
 			description: "测试1",
 			location: "网页",
 			date: "2026-07-21",
 			tags: ["异次元", "测试", "相册"],
-			password: "123",
-			passwordHint: "测试123",
 		},
 	],
 
