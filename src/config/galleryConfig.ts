a90e321199147fb27ae605ebd0f4b1eca9d82c28
Ghost <2528639663@qq.com>
2026-07-23 15:13:10 +0800

    gallery: create album "相册封面上传测试" (相册封面上传测试)

--- src/config/galleryConfig.ts
+++ src/config/galleryConfig.ts
@@ -23,6 +23,14 @@ export const galleryConfig: GalleryConfig = {
 			date: "2026-07-21",
 			tags: ["异次元", "测试", "相册"],
 		},
+			{
+			id: "相册封面上传测试",
+			name: "相册封面上传测试",
+			description: "封面设置ghost",
+			location: "hub20260101",
+			date: "2026-01-01",
+			tags: ["测试", "封面", "时间", "设置"],
+		},
 	],
 
 	// 瀑布流最小列宽(px)，浏览器根据容器宽度自动计算列数，默认 240
